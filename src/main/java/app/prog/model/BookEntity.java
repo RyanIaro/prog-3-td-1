0f5d91422c3e8702fdb77cfe5577677ffb31d85b
RyanIaro <hei.ryan.3@gmail.com>
2022-12-13 07:58:47 +0300

    fix: ManyToOne annotation for BookEntity id

--- src/main/java/app/prog/model/BookEntity.java
+++ src/main/java/app/prog/model/BookEntity.java
@@ -21,7 +21,7 @@ public class BookEntity {
     private String title;
     //TODO-6: join BookEntity with AuthorEntity and make the correct change so that the API does not change
     // Must know : a book can have only one author but an author can have many books
-    @OneToOne
+    @ManyToOne
     @JoinColumn(name = "id")
     private String author;
     private Integer pageNumber;
